0ea78f531c3e17843d671b406b22513d4a4d51a5
jhojanperlaza <jhojanperlaza@gmail.com>
2022-04-19 21:30:31 -0500

    [change] betty

--- shell.h
+++ shell.h
@@ -1,11 +1,11 @@
 #ifndef SHELL_H
 #define SHELL_H
 /*
-* File: main.h
-* Auth: Andres Ocaña - Jhojan Perlaza
-* Desc: Header file containing declarations for all functions
-* used in the _printf.
-*/
+ * File: main.h
+ * Auth: Andres Ocaña - Jhojan Perlaza
+ * Desc: Header file containing declarations for all functions
+ * used in the _printf.
+ */
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -16,14 +16,14 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <errno.h>
-#include<stdbool.h>
+#include <stdbool.h>
 
 extern char **environ;
 
 /**
  * struct get - helps to match the parameters
  *
- * @type: type of fuction 
+ * @shape: type of fuction
  * @f: The function associated
  */
 typedef struct get
@@ -34,13 +34,14 @@ typedef struct get
 
 char *_strcat(char *dest, char *src);
 char **get_arguments(char *buffer, int cont);
-int execute(char **arguments, char *copy, char *buffer, char **name_file, int cont);
+int execute(char **arguments, char *copy, char *buffer,
+char **name_file, int cont);
 char *get_PATH(char *argv);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 char *_strdup(char *str);
 int fun_exit(char **comand, char *copy, char *buffer);
-int (*match_fun(char *comands))(char **comand,char *copy, char *buffer );
+int (*match_fun(char *comands))(char **comand, char *copy, char *buffer);
 int fun_env(char **arguments, char *copy, char *buffer);
 void print_error(char **name_file, char *copy, int cont);
 #endif
